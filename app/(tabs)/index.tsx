b0ca61c49d6142cf999b51fc173249b3670446a8
Manodya Dissanayake <pasindumanodyadissanayake@gmail.com>
2025-11-25 14:20:21 +0530

    refactor: enhance SearchBar component with props for placeholder and onPress functionality

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,8 +2,12 @@ import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
 import { Image, ScrollView, View } from "react-native";
+import { useRouter } from "expo-router";
+
 
 export default function Index() {
+  const router = useRouter();
+
   return (
     <View className="flex-1 bg-primary">
       <Image source= {images.bg} className="absolute w-full z-0"/>
@@ -13,7 +17,10 @@ export default function Index() {
 
       <Image source = {icons.logo} className="w-12 h-10 mx-auto mb-5 mt-20"/>
         <View className="flex-1 mt-5">
-          <SearchBar />
+          <SearchBar 
+            onPress={() => router.push("/search")}
+            placeholder="Search for a movie"
+          />
         </View>
     </ScrollView>
     </View>
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,14 +1,17 @@
 import { icons } from "@/constants/icons";
 import React from "react";
 import { Image, TextInput, View } from "react-native";
-
-const SearchBar = () => {
+interface Props{
+    placeholder: string;
+    onPress: () => void;
+}
+const SearchBar = ({placeholder, onPress}: Props) => {
   return (
     <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
         <Image source = {icons.search} className="size-5" resize-mode= "contain" tintColor="#ab8bff"></Image>
         <TextInput
-            onPress={() => {}}
-            placeholder="Search"
+            onPress={onPress}
+            placeholder={placeholder}
             value=""
             onChangeText={() => {}}
             placeholderTextColor="#a8b5db"
