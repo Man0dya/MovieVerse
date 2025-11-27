f1f0b2fb6024a88f09e56774dacfb7cfb645e122
Manodya Dissanayake <pasindumanodyadissanayake@gmail.com>
2025-11-27 14:31:58 +0530

    feat: enhance search functionality and UI components, add Appwrite integration for search tracking

---
+++ services/appwrite.ts
@@ -0,0 +1,24 @@
+//track the searches made by users
+import { Client, Databases, Query } from 'react-native-appwrite';
+
+const DATABASE_ID = process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!;
+const COLLECTION_ID = process.env.EXPO_PUBLIC_APPWRITE_COLLECTION_ID!;
+
+const client = new Client()
+      .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
+      .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!); // Your project ID
+
+const databases = new Databases(client);
+
+
+export const updateSearchCount = async (query: string, movie:Movie) => {
+    const result = await databases.listDocuments(DATABASE_ID, COLLECTION_ID, [
+        Query.equal('searchTerm', query)
+    ]);
+    
+    
+    //check if a record of that search has already been stored
+     //if a document is found, search count is incremented by 1
+        //if no document is found, create a new document with search count set to 1
+    
+}
