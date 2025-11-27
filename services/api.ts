74b4786a98289fdacbcaea12e61530efcb407ad6
Manodya Dissanayake <pasindumanodyadissanayake@gmail.com>
2025-11-28 03:09:30 +0530

    feat: implement movie details fetching and display in Details component

--- services/api.ts
+++ services/api.ts
@@ -23,3 +23,23 @@ export const fetchMovies = async ({query}: {query: string}) => {
   const data = await response.json();
   return data.results;
 };
+
+
+export const fetchMovieDetails = async (movieId: string):Promise<MovieDetails> => {
+  try {
+    const response = await fetch(`${TMBD_CONFIG.BASE_URL}/movie/${movieId}?api_key=${TMBD_CONFIG.API_KEY}`,{
+      method: 'GET',
+      headers: TMBD_CONFIG.Headers,
+    });
+    if (!response.ok) {
+      throw new Error('Failed to fetch movie details');
+    }
+    const data = await response.json();
+
+    return data as MovieDetails;
+    
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
